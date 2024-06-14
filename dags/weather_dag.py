917608864ecacddc0baa195f990385f4006e8cd2
Susmi94 <92520291+Susmi94@users.noreply.github.com>
2024-06-14 09:35:19 -0400

    Add files via upload

---
+++ dags/weather_dag.py
@@ -0,0 +1,84 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+import json
+
+
+def kelvin_to_fahrenheit(temp_in_kelvin):
+    temp_in_fahrenheit = (temp_in_kelvin - 273.15) * (9/5) + 32
+    return temp_in_fahrenheit
+
+
+def transform_load_data(task_instance):
+    data = task_instance.xcom_pull(task_ids="extract_weather_data")
+    city = data["name"]
+    weather_description = data["weather"][0]['description']
+    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
+    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
+    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
+    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    # time_of_record = datetime.utcfromtimestamp(data['dt'] + data['timezone'])
+    # sunrise_time = datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    # sunset_time = datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+
+    transformed_data = {"City": city,
+                        "Description": weather_description,
+                        "Temperature (F)": temp_farenheit,
+                        "Feels Like (F)": feels_like_farenheit,
+                        "Minimun Temp (F)":min_temp_farenheit,
+                        "Maximum Temp (F)": max_temp_farenheit,
+                        "Pressure": pressure,
+                        "Humidty": humidity,
+                        "Wind Speed": wind_speed
+                        # "Time of Record": time_of_record,
+                        # "Sunrise (Local Time)":sunrise_time,
+                        # "Sunset (Local Time)": sunset_time                        
+                        }
+    transformed_data_list = [transformed_data]
+    df_data = pd.DataFrame(transformed_data_list)
+    aws_credentials = {"key": "xxxxxxxJRVFHA4VK", "secret": "xxxxxxxxxxxxxxxxu41ByoFI7pS", "token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxzuquHRf5SV0mmjXT1QYWSQ=="}
+
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y%H%M%S")
+    dt_string = 'current_weather_data_portland_' + dt_string
+    # df_data.to_csv(f"{dt_string}.csv", index=False) ###, storage_options=aws_credentials)
+    df_data.to_csv(f"s3://openweatherapi-s3/{dt_string}.csv", index=False, storage_options=aws_credentials)
+
+
+with DAG(dag_id="weather_dag",
+         schedule_interval= "@daily", ## timedelta(minutes=2),  ## # Schedule to run every 2 minutes ##
+         default_args={
+             "owner": "airflow",
+             "start_date": datetime(2020, 11, 1),
+             "retries": 1,
+             "retry_delay": timedelta(minutes=1)
+         },
+         catchup=False) as dag:
+
+        is_weather_api_ready = HttpSensor(
+        task_id ='is_weather_api_ready',
+        http_conn_id='weathermap_api',
+        endpoint='/data/2.5/weather?q=Waterloo&APPID=2cbe105e0d6e92b656a468bd53823442'
+        )
+
+        extract_weather_data = SimpleHttpOperator(
+        task_id = 'extract_weather_data',
+        http_conn_id = 'weathermap_api',
+        endpoint='/data/2.5/weather?q=Waterloo&APPID=2cbe105e0d6e92b656a468bd53823442',
+        method = 'GET',
+        response_filter= lambda r: json.loads(r.text),
+        log_response=True
+        )
+
+        transform_load_weather_data = PythonOperator(
+        task_id= 'transform_load_weather_data',
+        python_callable=transform_load_data
+        )
+
+        is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
